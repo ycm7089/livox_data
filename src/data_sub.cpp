852fab569e9ac8430810e92150590fd576324d3e
ycm7089 <ycm7089@naver.com>
2022-10-07 16:52:15 +0900

    size nice

--- src/data_sub.cpp
+++ src/data_sub.cpp
@@ -23,7 +23,7 @@ typedef struct
 void lidar_cb(const livox_ros_driver::CustomMsg::ConstPtr & msg)
 {
     Livox_data_Info livox_info;
-    // ROS_INFO("%.3f", msg -> points.x);
+
     livox_info.LiDARInfo_timebase = msg -> timebase;
     livox_info.LiDARInfo_point_num = msg -> point_num;
     for(int i = 0; i < 3; i++)
@@ -31,10 +31,11 @@ void lidar_cb(const livox_ros_driver::CustomMsg::ConstPtr & msg)
         livox_info.LiDARInfo_rsvd[i] = msg -> rsvd[i];
         // ROS_INFO("%d", livox_info.LiDARInfo_rsvd[i]);
     }
-    // 왜 0이지?
-    // ROS_INFO("points size is %.3f", sizeof(msg->points)) ; 
 
-    for(int i = 0; i < 7; i++)
+    ROS_INFO("points size is %d", sizeof(msg->points)) ;  // 24 
+    int cnt = 0;
+
+    for(int i = 0; i < sizeof(msg->points); i++)
     {
         livox_info.LiDARInfo_offset_time = msg -> points[i].offset_time;
         livox_info.LiDARInfo_x = msg -> points[i].x;
@@ -44,9 +45,14 @@ void lidar_cb(const livox_ros_driver::CustomMsg::ConstPtr & msg)
         livox_info.LiDARInfo_tag = msg -> points[i].tag;
         livox_info.LiDARInfo_line = msg -> points[i].line;
 
+        // ROS_INFO("points size is %d", sizeof(msg->points[i])) ; 
+
         ROS_INFO("x %.3f", livox_info.LiDARInfo_x);
-        ROS_INFO("y %.3f", livox_info.LiDARInfo_y);
-        ROS_INFO("z %.3f", livox_info.LiDARInfo_z);
+        // ROS_INFO("y %.3f", livox_info.LiDARInfo_y);
+        // ROS_INFO("z %.3f", livox_info.LiDARInfo_z);
+        cnt = cnt + 1;
+        // ROS_INFO("%d", cnt);
+
     }    
     // ROS_INFO("%d", livox_info.LiDARInfo_timebase);
     // ROS_INFO("%d", livox_info.LiDARInfo_point_num); //2400개
